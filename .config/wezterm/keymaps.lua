f884b9be7b1163f88ec58ed359b77ab78c7b8f81
Matej Bransky <matej.bransky@ataccama.com>
2023-04-20 18:38:13 +0200

    add keybinding for setting tab title

--- .config/wezterm/keymaps.lua
+++ .config/wezterm/keymaps.lua
@@ -71,4 +71,20 @@ table.insert(keys, {
 	}),
 })
 
+table.insert(keys, {
+	key = "E",
+	mods = "CTRL|SHIFT",
+	action = wezterm.action.PromptInputLine({
+		description = "Enter new name for tab",
+		action = wezterm.action_callback(function(window, _, line)
+			-- line will be `nil` if they hit escape without entering anything
+			-- An empty string if they just hit enter
+			-- Or the actual line of text they wrote
+			if line then
+				window:active_tab():set_title(line)
+			end
+		end),
+	}),
+})
+
 return keys
