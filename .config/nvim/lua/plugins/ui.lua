ce718ed18d0bc8765d613104810f84dc3f66dbc8
Matej Bransky <matej.bransky@ataccama.com>
2023-05-09 22:32:18 +0200

    use better search algorithm in telescope (zf-native)

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -202,4 +202,10 @@ return {
       },
     },
   },
+  {
+    "natecraddock/telescope-zf-native.nvim",
+    config = function()
+      require("telescope").load_extension("zf-native")
+    end,
+  },
 }
