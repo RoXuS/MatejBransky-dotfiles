4b2ad8c88fa23110dd18ea8272041f5d3b03570a
Matej Bransky <matej.bransky@ataccama.com>
2023-07-18 09:52:34 +0200

    remove unused nvim mappings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,44 +10,27 @@ local searchSelected = [[y/\V<C-r>=escape(@", '')<CR><CR>]]
 
 -- Show diag. in floating window
 vim.keymap.set("n", "<leader>k", vim.diagnostic.open_float)
--- delete to blackhole register
-vim.keymap.set("n", "<leader>d", '"_d')
--- replace a word under the cursor
-vim.keymap.set("n", "<leader>r", "viw" .. searchSelected .. "``cgn")
 
 -- document added keymappings in normal mode
 whichKey.register({
-  ["<leader>d"] = {
-    name = "delete to blackhole register",
-  },
   ["<leader>k"] = "Show diag. in a floating window.",
-  ["<leader>r"] = "Replace a word under the cursor",
 }, {
   mode = "n",
 })
 
--- replace a selection and keep the original value in the register
-vim.keymap.set("x", "<leader>p", [["_dP]])
 -- change a selection (repeatable action)
-vim.keymap.set("v", "<leader>rr", searchSelected .. "``cgn")
--- delete to blackhole register
-vim.keymap.set("v", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>r", searchSelected .. "``cgn")
 -- search within visual selection
 vim.keymap.set("v", "<leader>/", "<esc>/\\%V")
 
--- substitution in visual selection
-vim.keymap.set("x", "<leader>rv", [[:s/\%V]])
+-- substitute in visual selection
+vim.keymap.set("x", "<leader>v", [[:s/\%V]])
 
 -- document added keymappings in visual/select mode
 whichKey.register({
   ["<leader>/"] = "Search within the selection",
-  ["<leader>r"] = {
-    name = "replace",
-    v = "Substite in the visual selection",
-    r = "Change the selection (repeatable action for other occurences)",
-  },
-  ["<leader>p"] = "Replace the selected",
-  ["<leader>d"] = "Delete to blackhole register",
+  ["<leader>v"] = "Substite in the visual selection",
+  ["<leader>r"] = "Change the selection (repeatable action for other occurences)",
 }, {
   mode = "v",
 })
