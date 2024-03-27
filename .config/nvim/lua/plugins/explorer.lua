6d1b17d3913ed0cc6764a0f0afc22f682e751e71
Matej Bransky <matej.bransky@ataccama.com>
2024-03-27 08:29:30 +0100

    nvim: add LSP ops into the explorer

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -296,4 +296,16 @@ return {
       },
     },
   },
+
+  -- connect operations to LSP
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
